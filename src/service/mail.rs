fc65cb2eabe5a07d2853b3299346c7a2f4bc0066
smokingplaya <smokingplaya@yandex.ru>
2025-03-27 03:31:12 +0300

    0.1.2 :: update adjust-rs to 0.1.26, now we use load_env!, async mail sending

--- src/service/mail.rs
+++ src/service/mail.rs
@@ -1,15 +1,10 @@
-use std::env;
 use anyhow::anyhow;
 use axum::Json;
-use adjust::response::{HttpMessage, HttpResult};
-use lazy_static::lazy_static;
+use adjust::{load_env, response::{HttpMessage, HttpResult}};
 use lettre::{message::header::ContentType, Message as SmtpMessage, SmtpTransport, Transport};
 use serde::{Deserialize, Serialize};
 
-lazy_static! {
-  static ref EMAIL_ADDRESS: String = env::var("SMTP_USER")
-    .expect("The SMTP_USER environment variable was not found!");
-}
+load_env!(EMAIL_ADDRESS);
 
 pub struct MailService;
 
@@ -25,17 +20,34 @@ impl MailService {
     message: Message,
     transport: SmtpTransport
   ) -> HttpResult<HttpMessage> {
-    let msg = SmtpMessage::builder()
-      // по всей видимости, EMAIL_ADDRESS должен быть x@domain.com
-      .from(EMAIL_ADDRESS.parse().map_err(|e| anyhow!("Не получилось запарсить EMAIL_ADDRESS: {e}"))?)
-      .to(message.to.parse().map_err(|e| anyhow!("Не получилось запарсить поле \"to\": {e}"))?)
-      .subject(message.subject)
-      .header(ContentType::TEXT_HTML)
-      .body(message.body)
-      .map_err(|e| anyhow!("Не получилось собрать SmtpMessage: {e}"))?;
+    tokio::spawn(async move {
+      let msg = SmtpMessage::builder()
+        // по всей видимости, EMAIL_ADDRESS должен быть x@domain.com
+        .from(EMAIL_ADDRESS.parse().map_err(|e| anyhow!("Не получилось запарсить EMAIL_ADDRESS: {e}"))?)
+        .to(message.to.parse().map_err(|e| anyhow!("Не получилось запарсить поле \"to\": {e}"))?)
+        .subject(message.subject)
+        .header(ContentType::TEXT_HTML)
+        .body(message.body)
+        .map_err(|e| anyhow!("Не получилось собрать SmtpMessage: {e}"));
 
-    transport.send(&msg)
-      .map_err(|e| anyhow!("Не получилось отправить сообщение через SMTP: {e}"))?;
+      if let Err(err) = msg {
+        log::error!("{err}");
+
+        return Err(err)
+      }
+
+      // safe unwrap
+      let sended = transport.send(&msg.unwrap())
+        .map_err(|e| anyhow!("Не получилось отправить сообщение через SMTP: {e}"));
+
+      if let Err(err) = sended {
+        log::error!("{err}");
+      }
+
+      // короче это нужно для указания return type = anyhow::Result
+      let result: anyhow::Result<()> = Ok(());
+      result
+    });
 
     Ok(Json(HttpMessage {
       message: String::from("Сообщение было успешно отправлено.")
